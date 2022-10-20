91d9cf7cb0bfe948ddcd607251e33fbf078ab47c
saitharun996 <saitharun996@gmail.com>
2022-10-20 11:52:10 +0530

    new classes are added

---
+++ src/main/java/BashPackage/TestData.java
@@ -0,0 +1,12 @@
+package BashPackage;
+
+public class TestData {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Username : Sai Tharun");
+		System.out.println("Password : @tharun123");
+		
+	}
+
+}
